abe7cfed40a3ca20838380a1558a788ba4d0d32a
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2021-10-23 03:41:58 -0400

    Add files via upload

--- Arduino Password Protected Door Lock Security System.cpp
+++ Arduino Password Protected Door Lock Security System.cpp
@@ -1,118 +1,185 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
-#include <Servo.h>
-Servo myservo;
-LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
-#define Password_Lenght 7 // Give enough room for six chars + 
-NULL char
-int pos = 0; // variable to store the servo position
-char Data[Password_Lenght]; // 6 is the number of chars it can 
-hold + the null char = 7
-char Master[Password_Lenght] = "123456";
-byte data_count = 0, master_count = 0;
-bool Pass_is_good;
-char customKey;
-const byte ROWS = 4;
-const byte COLS = 3;
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
- {'1', '2', '3'},
- {'4', '5', '6'},
- {'7', '8', '9'},
- {'*', '0', '#'}
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
-bool door = true;
-byte rowPins[ROWS] = {1, 2, 3, 4}; //connect to the row pinouts 
-of the keypad
-byte colPins[COLS] = {5, 6, 7}; //connect to the column pinouts 
-of the keypad
-Keypad customKeypad( makeKeymap(keys), rowPins, colPins, ROWS, 
-COLS); //initialize an instance of class NewKeypad
-void setup()
-{
- myservo.attach(9);
- ServoClose();
- lcd.begin(16, 2);
- lcd.print(" Arduino Door");
- lcd.setCursor(0, 1);
- lcd.print("--Look project--");
- delay(3000);
- lcd.clear();
+byte rowPins[ROWS] = {A0, A1, A2, A3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A4, A5, 2}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+// 16x2 LCD 
+#define rs 10 
+#define en 9 
+#define d4 6  
+#define d5 5  
+#define d6 4 
+#define d7 3 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+String password = "1234";
+String mypassword;
+
+int redled = 12;
+int lock = 13;
+
+int counter = 0; 
+int attempts = 0; 
+int max_attempts = 3; 
+
+void setup(){
+  Serial.begin(9600);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  
+  pinMode(redled, OUTPUT);
+  pinMode(lock, OUTPUT);
+  
+  digitalWrite(redled, LOW);
+  digitalWrite(lock, LOW);
+  
+  Serial.println("enter password");
+    lcd.print("Enter Password:");
 }
+  
 void loop()
 {
- if (door == 0)
- {
- customKey = customKeypad.getKey();
- if (customKey == '#')
- {
- lcd.clear();
- ServoClose();
- lcd.print(" Door is close");
- delay(3000);
- door = 1;
- }
- }
- else Open();
+  
+ keypadfunction();
+ 
 }
-void clearData()
+
+void keypadfunction()
 {
- while (data_count != 0)
- { // This can be used for any array size,
- Data[data_count--] = 0; //clear array for new data
- }
- return;
+ char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+    counter = counter + 1; 
+    lcd.setCursor(counter, 1);
+    lcd.print("*");
+  }
+  if (key == '1')
+  {
+
+    mypassword = mypassword + 1;   
+  }
+  
+    if (key == '2')
+  {
+
+    mypassword = mypassword + 2;  
+  }
+  
+  if (key == '3')
+  {
+ 
+    mypassword = mypassword + 3; 
+  }
+  
+   if (key == '4')
+  {
+  
+    mypassword = mypassword + 4;  
+  }
+  
+  if (key == '5')
+  {
+  
+    mypassword = mypassword + 5;
+  }
+  
+   if (key == '6')
+  {
+   
+    mypassword = mypassword + 6; 
+  }
+  
+   if (key == '7')
+  {
+
+    mypassword = mypassword + 7; 
+  }
+
+   if (key == '8')
+  {
+
+    mypassword = mypassword + 8; 
+  }
+  
+  if (key == '9')
+  {
+
+    mypassword = mypassword + 9;
+  }
+             
+                 if (key == '0')
+  {
+
+    mypassword = mypassword + 0; 
+  }
+  
+  
+        if (key == '*')
+  {
+    Serial.println(mypassword); 
+    
+if ( password == mypassword )
+{
+lcd.clear(); 
+lcd.println("Welcome To");
+lcd.setCursor(0,1);
+lcd.println("ElectroniClinic");
+digitalWrite(lock, HIGH);
+delay(5000); 
+digitalWrite(lock,LOW);
+mypassword = ""; 
+counter = 0; 
+lcd.clear();
+lcd.setCursor(0,0); 
+lcd.println("Enter password");
 }
-void ServoOpen()
+else
 {
- for (pos = 180; pos >= 0; pos -= 5) { // goes from 0 degrees 
-to 180 degrees
- // in steps of 1 degree
- myservo.write(pos); // tell servo to go to 
-position in variable 'pos'
- delay(15); // waits 15ms for the servo 
-to reach the position
- }
+Serial.println("wrong");
+digitalWrite(lock, LOW);
+attempts = attempts + 1; 
+if (attempts >= max_attempts )
+{
+  lcd.clear();
+  lcd.setCursor(0,0); 
+  lcd.print("Locked Out");
+  
+digitalWrite(redled, HIGH);
+delay(5000); 
+digitalWrite(redled, LOW); 
+attempts = 0; 
+
 }
-void ServoClose()
-{
- for (pos = 0; pos <= 180; pos += 5) { // goes from 180 degrees 
-to 0 degrees
- myservo.write(pos); // tell servo to go to 
-position in variable 'pos'
- delay(15); // waits 15ms for the servo 
-to reach the position
- }
+mypassword = ""; 
+counter = 0; 
+lcd.clear(); 
+lcd.setCursor(0,0); 
+lcd.print("Wrong Password");
+delay(1000);
+
+lcd.setCursor(0,1); 
+lcd.print("max attempts 3");
+delay(1000);
+
+lcd.clear(); 
+lcd.println("Enter password");
+lcd.setCursor(0,1); 
 }
-void Open()
-{
- lcd.setCursor(0, 0);
- lcd.print(" Enter Password");
- customKey = customKeypad.getKey();
- if (customKey) // makes sure a key is actually pressed, equal 
-to (customKey != NO_KEY)
- {
- Data[data_count] = customKey; // store char into data array
- lcd.setCursor(data_count, 1); // move cursor to show each 
-new char
- lcd.print(Data[data_count]); // print char at said cursor
- data_count++; // increment data array by 1 to store new 
-char, also keep track of the number of chars entered
- }
- if (data_count == Password_Lenght - 1) // if the array index 
-is equal to the number of expected chars, compare data to master
- {
- if (!strcmp(Data, Master)) // equal to (strcmp(Data, Master) 
-== 0)
- {
- lcd.clear();
- ServoOpen();
- lcd.print(" Door is Open");
- door = 0;
- }
- lcd.clear();
- lcd.print(" Wrong Password");
- delay(1000);
- door = 1;
- }
- clearData();
+    
+  }  
+  
+  
 }
